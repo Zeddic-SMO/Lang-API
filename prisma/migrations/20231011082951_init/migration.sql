1465652e842e20feaa7d9bbb8645dc7f8cc93704
Zeddic-smo <samuel93ortil@gmail.com>
2023-10-11 11:36:43 +0100

    Prisma db deployed..

--- prisma/migrations/20231011082951_init/migration.sql
+++ prisma/migrations/20231011082951_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'inactive',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL
 );
